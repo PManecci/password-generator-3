5c82ddd6a83b214af10e26900d7f87a07c2f0f62
manecci <pmanecci@gmail.com>
2021-11-06 10:07:11 -0400

    HTML Form Inputs

--- script.js
+++ script.js
@@ -2,16 +2,40 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+////var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+ // var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+ // passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+
+
+//When the user clicks the generate button they should be prompted to select password criteria
+
+//User specifies length of password between 8 and 128 characters
+const characterRange = document.getElementById('characterRange')
+const characterNumber = document.getElementById('charachterNumber')
+
+characterRange.addEventListener('input', syncCharacterAmount)
+characterNumber.addEventListener('input', syncCharacterAmount)
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterRange.value = value
+  characterNumber.value = value
+};
+
+//User specifies character types needed (lowercase, uppercase, numeric and/or special charaters)
+
+//Confirm user input and that at least one character type has been selected
+
+//Generate Password
+
+//Display Password
