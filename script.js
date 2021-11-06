a4b918ac5c6abf2a9942b89a67969967f027935f
manecci <pmanecci@gmail.com>
2021-11-06 11:08:52 -0400

    Password Generation & Display Functional

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-// Assignment code here
-
-
 // Get references to the #generate element
-////var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-//function writePassword() {
- // var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-
- // passwordText.value = password;
-
-//}
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-
-
-//When the user clicks the generate button they should be prompted to select password criteria
+var generateBtn = document.querySelector("#generate");
 
 //User specifies length of password between 8 and 128 characters
 const characterRange = document.getElementById('characterRange')
-const characterNumber = document.getElementById('charachterNumber')
+const characterNumber = document.getElementById('characterNumber')
 
 characterRange.addEventListener('input', syncCharacterAmount)
 characterNumber.addEventListener('input', syncCharacterAmount)
@@ -30,12 +12,58 @@ function syncCharacterAmount(e) {
   const value = e.target.value
   characterRange.value = value
   characterNumber.value = value
-};
+}
 
 //User specifies character types needed (lowercase, uppercase, numeric and/or special charaters)
+const includeUpperCaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const passwordDisplay = document.getElementById('password')
 
-//Confirm user input and that at least one character type has been selected
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+  ).concat(
+    arrayFromLowToHigh(91, 96)
+  ).concat(
+    arrayFromLowToHigh(123, 126)
+  )
 
 //Generate Password
+generateBtn.addEventListener("click", e => {
+  e.preventDefault()
+  const characterAmount = characterNumber.value
+  const includeUppercase = includeUpperCaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount,
+    includeUppercase, includeNumbers, includeSymbols);
+  passwordDisplay.innerText = password
+})
+
+function generatePassword(characterAmount, 
+  includeUppercase, includeNumbers, includeSymbols) {
+    let charCodes = LOWERCASE_CHAR_CODES
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+    const passwordCharacters = []
+    for (let i = 0; i < characterAmount; i++) {
+      const characterCode = charCodes[Math.floor(Math.random() *
+        charCodes.length)]
+      passwordCharacters.push(String.fromCharCode(characterCode))
+    }
+    return passwordCharacters.join('')
+  }
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i<= high; i++) {
+    array.push(i)
+  }
+  return array
+}
 
 //Display Password
